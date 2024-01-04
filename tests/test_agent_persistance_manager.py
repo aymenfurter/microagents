68a8abba8c152594a3f11276c9b69930f1363115
Aymen Furter <aymen.furter@outlook.com>
2024-01-04 17:18:31 +0000

    feat: making agents persistant

--- tests/test_agent_persistance_manager.py
+++ tests/test_agent_persistance_manager.py
@@ -3,10 +3,13 @@ from unittest.mock import Mock, patch
 from agents.agent_serializer import AgentSerializer
 from agents.agent_persistence_manager import AgentPersistenceManager
 from integrations.sqlite_agent_persistence import SQLiteAgentPersistence
+from integrations.openaiwrapper import OpenAIAPIWrapper
 
 class TestAgentPersistenceManager(unittest.TestCase):
 
     def setUp(self):
+        self.mock_agent_lifecycle = Mock()
+        self.mock_openai_wrapper = OpenAIAPIWrapper("api_key")
         self.mock_persistence = Mock(spec=SQLiteAgentPersistence)
         self.manager = AgentPersistenceManager(
             db_filename=":memory:"
@@ -38,14 +41,15 @@ class TestAgentPersistenceManager(unittest.TestCase):
         mock_agent_dict = {'purpose': purpose, 'data': 'some_data'}
         self.mock_persistence.fetch_agent.return_value = mock_agent_dict
 
-        with patch('agents.agent_serializer.AgentSerializer.from_dict') as mock_from_dict:
-            self.manager.load_agent(purpose)
 
-        mock_from_dict.assert_called_once_with(mock_agent_dict)
+        with patch('agents.agent_serializer.AgentSerializer.from_dict') as mock_from_dict:
+            self.manager.load_agent(purpose, self.mock_agent_lifecycle, self.mock_openai_wrapper)
+
+        mock_from_dict.assert_called_once_with(mock_agent_dict, self.mock_agent_lifecycle, self.mock_openai_wrapper)
 
     def test_load_agent_non_existing(self):
         self.mock_persistence.fetch_agent.return_value = None
-        result = self.manager.load_agent('non_existent')
+        result = self.manager.load_agent('non_existent', self.mock_agent_lifecycle, self.mock_openai_wrapper)
 
         self.assertIsNone(result)
 
@@ -54,7 +58,7 @@ class TestAgentPersistenceManager(unittest.TestCase):
         self.mock_persistence.fetch_agent.side_effect = [{'purpose': 'purpose1', 'data': 'data1'}, {'purpose': 'purpose2', 'data': 'data2'}]
 
         with patch('agents.agent_serializer.AgentSerializer.from_dict') as mock_from_dict:
-            agents = self.manager.load_all_agents()
+            agents = self.manager.load_all_agents(self.mock_agent_lifecycle, self.mock_openai_wrapper)
 
         self.assertEqual(len(agents), 2)
         self.assertEqual(mock_from_dict.call_count, 2)
